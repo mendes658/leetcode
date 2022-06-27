a3aeba434d5247d5e57ff6ccd5c95033b1265117
Pedro Mendes <99758612+mendes658@users.noreply.github.com>
2022-06-27 00:17:36 -0300

    Create min_consecutive_cards.py

---
+++ min_consecutive_cards.py
@@ -0,0 +1,16 @@
+#https://leetcode.com/problems/minimum-consecutive-cards-to-pick-up/
+
+def minimumCardPickup(self, cards: List[int]) -> int:
+	dic = {}
+  mini = 10000000
+  for index, card in enumerate(cards):
+		if not card in dic.keys():
+				dic[card] = index
+		else:
+				sub = index-dic[card]+1
+				if sub < mini:
+						mini = sub
+				dic[card] = index
+if mini == 10000000:
+	return -1
+return mini
